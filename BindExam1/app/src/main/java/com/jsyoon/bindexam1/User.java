835eb3c19a1f0d7907e09f80946beb83867b42dc
jsyoon <jsyoon@shinwooend.co.kr>
2017-04-12 16:25:06 +0900

    Observable 객체 사용

--- BindExam1/app/src/main/java/com/jsyoon/bindexam1/User.java
+++ BindExam1/app/src/main/java/com/jsyoon/bindexam1/User.java
@@ -1,20 +1,38 @@
 package com.jsyoon.bindexam1;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 /**
  * Created by ShinwooEND on 2017-04-11.
  */
 
-public class User {
-    private final String firstName;
-    private final String lastName;
+public class User extends BaseObservable {
+    private String firstName;
+    private String lastName;
+
     public User(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+        setFirstName(firstName);
+        setLastName(lastName);
     }
+
+    public User() { }
+
+    @Bindable
     public String getFirstName() {
         return this.firstName;
     }
+    @Bindable
     public String getLastName() {
         return this.lastName;
     }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+        notifyPropertyChanged(BR.firstName);
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+        notifyPropertyChanged(BR.lastName);
+    }
+
 }
