0ba179ed0633ca7ac2ebe814e121129d9be2c9d5
jsyoon <jsyoon@shinwooend.co.kr>
2017-04-20 16:24:16 +0900

    savedInstanceState / onRestoreInstanceState

--- Twowaybinding/app/src/main/java/com/jsyoon/twowaybinding/MainActivity.java
+++ Twowaybinding/app/src/main/java/com/jsyoon/twowaybinding/MainActivity.java
@@ -7,8 +7,8 @@ import android.os.Bundle;
 import com.jsyoon.twowaybinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String ECHOTEST = "echotest";
-    private String mEchotest;
+    public static final String ECHO = "echotest";
+    private String mEcho;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +16,13 @@ public class MainActivity extends AppCompatActivity {
 
         if(savedInstanceState != null) {
             // Restore value of members from saved state
-            mEchotest = savedInstanceState.getString(ECHOTEST);
+            mEcho = savedInstanceState.getString(ECHO);
         } else {
             // Probably initialize members with default values for a new instance
+            mEcho = ECHO;
         }
         Echo echo = new Echo();
-        echo.text.set(mEchotest);
+        echo.text.set(mEcho);
 
         // layout에 있는 echo 변수 set
         binding.setEcho(echo);
@@ -29,8 +30,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
-        savedInstanceState.putString(ECHOTEST, mEchotest);
+        savedInstanceState.putString(ECHO, mEcho);
         // Always call the superclass so it can save the view hierarchy state
         super.onSaveInstanceState(savedInstanceState);
     }
+
+    /*
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.v(TAG, "Inside of onRestoreInstanceState");
+        startTime = (Calendar) savedInstanceState.getSerializable("starttime");
+    }
+    */
 }
