e4bfa74d4b6a9f797f76d27c7fa61aa902dc9f06
jsyoon <jsyoon@shinwooend.co.kr>
2017-04-25 18:49:30 +0900

    메인 activity에서 연결 구현

--- SettingPref/app/src/main/java/com/jsyoon/settingpref/MainActivity.java
+++ SettingPref/app/src/main/java/com/jsyoon/settingpref/MainActivity.java
@@ -1,18 +1,36 @@
 package com.jsyoon.settingpref;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.CheckedTextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private CheckedTextView setting1, setting2,setting3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setting1 = (CheckedTextView) findViewById(R.id.setting1);
+        setting2 = (CheckedTextView) findViewById(R.id.setting2);
+        setting3 = (CheckedTextView) findViewById(R.id.setting3);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        setting1.setChecked(sharedPreferences.getBoolean("setting1",true));
+        setting2.setChecked(sharedPreferences.getBoolean("setting2",true));
+        setting3.setChecked(sharedPreferences.getBoolean("setting3",true));
+
+        // Register the listener
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
     }
 
     /**
@@ -38,5 +56,16 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals("setting1")) {
+            setting1.setChecked(sharedPreferences.getBoolean(key,true));
+        } else if (key.equals("setting2")) {
+            setting2.setChecked(sharedPreferences.getBoolean(key,true));
+        } else if (key.equals("setting3")) {
+            setting3.setChecked(sharedPreferences.getBoolean(key,true));
+        }
     }
 }
