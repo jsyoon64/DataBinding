206fc556bbdc2dfd27a42ab69bd1713fecd86b8a
jsyoon <jsyoon@shinwooend.co.kr>
2017-04-25 14:37:40 +0900

    Shared Preference Read / Write

--- SharedPref/app/src/main/java/com/jsyoon/sharedpref/MainActivity.java
+++ SharedPref/app/src/main/java/com/jsyoon/sharedpref/MainActivity.java
@@ -1,13 +1,58 @@
 package com.jsyoon.sharedpref;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText ed1,ed2,ed3;
+    Button b1;
+
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String Name = "nameKey";
+    public static final String Pass = "passKey";
+    public static final String Email = "emailKey";
+
+    SharedPreferences sharedpreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ed1=(EditText)findViewById(R.id.editText);
+        ed2=(EditText)findViewById(R.id.editText2);
+        ed3=(EditText)findViewById(R.id.editText3);
+
+        b1=(Button)findViewById(R.id.button);
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+        // 저장한 값이 있으면 읽어오고 그렇지 않으면 ""
+        ed1.setText(sharedpreferences.getString(Name,""));
+        ed2.setText(sharedpreferences.getString(Pass,""));
+        ed3.setText(sharedpreferences.getString(Email,""));
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String n  = ed1.getText().toString();
+                String ph  = ed2.getText().toString();
+                String e  = ed3.getText().toString();
+
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+
+                editor.putString(Name, n);
+                editor.putString(Pass, ph);
+                editor.putString(Email, e);
+                editor.commit();
+                Toast.makeText(MainActivity.this,"Thanks",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
