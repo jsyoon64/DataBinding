158b5d3e975b898904b55fca5f77418fe9abfdcc
jsyoon <jsyoon@shinwooend.co.kr>
2017-06-26 10:39:43 +0900

    단순 명료

--- BindExam1/app/src/main/java/com/jsyoon/bindexam1/MainActivity.java
+++ BindExam1/app/src/main/java/com/jsyoon/bindexam1/MainActivity.java
@@ -10,9 +10,6 @@ import com.jsyoon.bindexam1.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    // case 1
-    //RecyclerView rvUserlist;
     ObservableArrayList<User> users1;
 
     @Override
@@ -20,31 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
-        init();
-
-        // case 2
-        UserdataAdaptor adapter = new UserdataAdaptor(this,users1);
-        binding.rvUserlists.setLayoutManager(new LinearLayoutManager(this));
-        binding.rvUserlists.setAdapter(adapter);
-        // --
-    }
-
-    private void init() {
-        users1 = new ObservableArrayList<User>();
-
-        // case 1
-        //rvUserlist = (RecyclerView) findViewById(R.id.rvUserlists);
-        //rvUserlist.setLayoutManager(new LinearLayoutManager(this));
 
         addusers();
 
-        // case 1
-        //UserdataAdaptor adapter = new UserdataAdaptor(this,users1);
-        //rvUserlist.setAdapter(adapter);
+        UserdataAdaptor adapter = new UserdataAdaptor(this,users1);
+        binding.rvUserlists.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvUserlists.setAdapter(adapter);
+
     }
 
     private void addusers()
     {
+        users1 = new ObservableArrayList<User>();
         for (int i=0;i<20;i++) {
             User user = new User("Test"+String.valueOf(i), "User"+String.valueOf(i));
             users1.add(user);
